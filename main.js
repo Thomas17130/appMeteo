d4aa762f8db6951e1bf200ceec620328fe14d5c6
Thomas <thomas.guillemette@campusdulac.com>
2022-05-11 16:38:04 +0200

    "add input"

---
+++ main.js
@@ -0,0 +1,11 @@
+let query = "";
+let apiKey = "12d0af88ad3576efac6006eb8a83d65e ";
+let url = `hhtp://api.weatherstack.com/current?access_key=${apiKey}&query=${query}`;
+
+const search = document.getElementById("search");
+
+search.addEventListener('submit', (event)=> {
+    event.preventDefault();
+    query = document.querySelector('#city').value;
+    console.log(query);
+}) 
